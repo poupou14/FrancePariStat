e46bac364995cc41f7d808499454f0228d7ae031
poupou14 <poulnais@gmail.com>
2016-11-21 22:59:32 +0100

    regexp

--- src/AnnabetParser.py
+++ src/AnnabetParser.py
@@ -4,6 +4,7 @@ import os,string, sys
 import urllib
 import urllib2
 import copy
+import re
 
 def mycounter():
 	if not hasattr(mycounter, "counter"):
@@ -21,41 +22,28 @@ class TorNet():
 
 	def __init__(self):
 		#self.__browser = "torify lynx -source"
-		self.__browser = "torify curl"
+                self.__browser = "elinks -source "
 
-	def openUrl(self, url_p, reboot_p):
-		counter_l = mycounter()
-		print "couter = %d" % counter_l
-		if counter_l % 10 == 0 or reboot_p : # on relance tor
-			outIO_l = os.popen("sudo /etc/init.d/tor restart")
-			outIO_l = os.popen("sudo /etc/init.d/privoxy restart")
-			print "redemarrage Tor : %s" % outIO_l
-		#cmd = os.popen(". /home/lili//SHELL/browser_proxy.ksh")
-		#else :
-		#cmd = os.popen(". $HOME/SHELL/browser_noproxy.ksh")
-		#print "Browser : %s" % browser_l
-		#print "Browser counter : %d" % counter_l
-		#print "Ouverture : %s" % self.__gameUrl
-		#url = urllib2.urlopen(self.__gameUrl, timeout = 5)
+        def openUrl(self, url_p):
 		cmd = os.popen("{!s} {!s}".format(self.__browser, url_p))
 		return cmd
 
 class AnnabetParser(HTMLParser): 
 
 	def __init__(self): 
-		self.__betweenTag = False
+                self.__betweenTag = False
+                self.__oddsFound = -1
 		self.__pinnacle = 0 
 		self.__bet365 = 0 
 		self.__myDate = ""
 		self.__team1 = ""
 		self.__team2 = ""
 		self.__game = ""
-		self.__gameUrl = ""
-		self.__httpAddress = "annabet.com"
+                self.__gameInfo = ""
+                self.__gameInfoFound = False
+                self.__httpAddress = "annabet.com"
 		self.__httpRootAddress = "https://annabet.com/en/soccerstats/"
-		self.__odds1 = ""
-		self.__oddsN = ""
-		self.__odds2 = ""
+                self.__odds = ["0.0", "0.0", "0.0"]
 
 
 	def setDate(self, date_p):
@@ -67,20 +55,20 @@ class AnnabetParser(HTMLParser):
 	def setTeam2(self, val) :
 		self.__team2 = val
 
-	def getGameUrl(self) :
+        def readGameInfo(self) :
 		essai_l = 1
-		self.__gameUrlFound = False
-		self.__gameUrl = ""
+                self.__gameInfoFound = False
+                self.__gameInfo = ""
 		team1_l = self.__team1
 		team2_l = self.__team2
 		browser_l = "links"
 		pair_l = True
-		while (not self.__gameUrlFound):
+                while (not self.__gameInfoFound):
 			query_l = ''.join((self.__httpAddress, ": "))
 			query_l = ''.join((query_l, self.__myDate))
 			query_l = ''.join((query_l, " "))
 			if essai_l == 1 :	# first try with team full name
-				self.__game = ''.join((self.__team1, " - "))
+                                self.__game = ''.join((self.__team1, " "))
 				self.__game = ''.join((self.__game, self.__team2))
 			elif essai_l == 2 :	# not first try
 				listTeam1_l = self.__team1.split()
@@ -95,7 +83,7 @@ class AnnabetParser(HTMLParser):
 					if len(listTeam2_l[i]) > len2_l :
 						len2_l = len(listTeam2_l[i])
 						team2_l = listTeam2_l[i]
-				self.__game = ''.join((team1_l, " - "))
+                                self.__game = ''.join((team1_l, " "))
 				self.__game = ''.join((self.__game, team2_l))
 			elif essai_l == 3 :	# not first try
 				listTeam1_l = self.__team1.split()
@@ -115,116 +103,122 @@ class AnnabetParser(HTMLParser):
 				self.__game = team2_l
 	
 			query_l = ''.join((query_l, self.__game))
-			filename = 'https://www.google.com/search?' + urllib.urlencode({'q': query_l })
+                        query_l = ''.join((query_l, " pinnacle"))
+                        self.__url = 'https://www.framabee.org/search?' + urllib.urlencode({'q': query_l })
 			pair_l = not pair_l
-			print "recherche : %s" % filename
-			browser_l = "lynx -listonly -width=1000"
-			commandeLine_l = "{!s} -dump {!s} | grep annabet.com".format(browser_l,filename)
-			cmd = os.popen(commandeLine_l)
-			output = cmd.read()
-			lineResult_l = output.splitlines()
-			#print "line result : %s" % lineResult_l
+                        print "recherche : %s" % self.__url
+                        browser_l = "lynx -source "
+                        commandeLine_l = "{!s} -dump {!s}".format(browser_l,self.__url)
+                        cmd = os.popen(commandeLine_l)
+                        self.__output = cmd.read()
+                        #print "output :\n%s" % self.__output
+                        lineResult_l = self.__output.splitlines()
+                        #print "line result : %s" % lineResult_l
 			cmd.close()
 			for line_l in lineResult_l :
 				#print "line : %s" % line_l
 				wordsResult_l = line_l.split(" ")
 				for word_l in wordsResult_l :
-					self.__gameUrlFound = word_l.find("annabet.com") != -1 and word_l.find("gamereport") != -1 
-					#self.__gameUrlFound = self.__gameUrlFound and word_l.find(team1_l) !=- 1 and word_l.find(team2_l) != -1 
-					if self.__gameUrlFound :
-						urlTab_l = word_l.split("/")
-						#print "res {!s} : {!s}".format(browser_l,urlTab_l[-1:][0])
-						#print urlTab_l
-						self.__gameUrl = ''.join((self.__httpRootAddress,urlTab_l[-1:][0]))
-						break
-				if self.__gameUrlFound : 
+                                        self.__gameInfoFound = word_l.find("annabet.com") != -1 and word_l.find("gamereport") != -1
+                                        if self.__gameInfoFound :
+                                                print "found"
+                                                break
+                                if self.__gameInfoFound :
 					break
 			if essai_l == 4 :
-				self.__gameUrlFound = True
+                                self.__gameInfoFound = True
 			essai_l += 1
-		if self.__gameUrl.find("http") == -1 :
-			self.__gameUrl = ''.join(("https://", self.__gameUrl))
-		if self.__gameUrl.find("html") == -1 :
-			self.__gameUrl = ''.join((self.__gameUrl, "html"))
-		#print "url : %s" % self.__gameUrl
-		return self.__gameUrl	
+                if self.__gameInfo.find("http") == -1 :
+                        self.__gameInfo = ''.join(("https://", self.__gameInfo))
+                if self.__gameInfo.find("html") == -1 :
+                        self.__gameInfo = ''.join((self.__gameInfo, "html"))
+                #print "url : %s" % self.__gameInfo
+                return essai_l
 
 
 
 	def getOdds(self) :
 		odds_l = []
 		notRead_l = 0
-		reboot_l = False
-		self.getGameUrl()
-		self.reset()
+                self.reset()
+                essai_l = self.readGameInfo()
 		while notRead_l <= 3 :
 			try :
-				myNet_l = TorNet()
-				cmd = myNet_l.openUrl(self.__gameUrl, reboot_l)
-				print "Lecture : %s" % self.__gameUrl
-				self.html = cmd.read()
+                                print "Lecture : %s" % self.__url
+                                #print self.__output
+                                self.html = self.__output
 				#self.html = url.read()
 				#url.close()
-				print "Fermeture : %s" % self.__gameUrl
+                                print "Fermeture : %s" % self.__url
 				self.html = filter(onlyascii, self.html)
 				self.feed(self.html)
-				if self.__odds1 != "" :
+                                if self.__odds[2] != "0.0" :
 					notRead_l = 4
-					reboot_l = False
 				else :
 					notRead_l += 1
-					reboot_l = True
 			#except IOError :
 			except IOError:
 				notRead_l += 1
-				print "pb with : %s" % self.__gameUrl
+                                print "pb with : %s" % self.__url
 				#exit
-		odds_l.append(self.__odds1)
-		odds_l.append(self.__oddsN)
-		odds_l.append(self.__odds2)
+                odds_l.append(self.__odds[0])
+                odds_l.append(self.__odds[1])
+                odds_l.append(self.__odds[2])
+                #a = raw_input("...")
 		return odds_l
 
 	def handle_starttag(self, tag, attrs):
-		if tag == "td" and len(attrs) == 1 and self.__pinnacle >= 1 :
-			self.__pinnacle += 1
-		elif tag == "td" and len(attrs) == 1 and self.__bet365 >= 1 :
-			self.__bet365 += 1
+                if self.__pinnacle == 0 and tag == "p" and len(attrs) == 1 :
+                        self.__pinnacle = 1
+                #elif tag == "td" and len(attrs) == 1 and self.__bet365 >= 1 :
+                        #self.__bet365 += 1
 		self.__betweenTag = True
 
 
 
 
 	def handle_data(self, data):
-		if self.__pinnacle == 0 and self.__betweenTag and data.find("PinnacleSport") != -1 :
-			print "Pinnacle Found"
-			self.__pinnacle = 1	
-		elif self.__pinnacle == 2  :
-			self.__odds1 = data
-			#print "Odds1 : %s" % data
-		elif self.__pinnacle == 3  :
-			self.__oddsN = data
-			#print "OddsN : %s" % data
-		elif self.__pinnacle == 4  :
-			self.__odds2 = data
-			#print "Odds2 : %s" % data
-			self.__pinnacle = -1
-		elif self.__bet365 == 0 and self.__pinnacle == 0 and self.__betweenTag and data.find("Bet365") != -1 :
-			print "Bet365 Found"
-			self.__bet365 = 1	
-		elif self.__bet365 == 2  :
-			self.__odds1 = data
-			#print "Odds1 : %s" % data
-		elif self.__bet365 == 3  :
-			self.__oddsN = data
-			#print "OddsN : %s" % data
-		elif self.__bet365 == 4  :
-			self.__odds2 = data
-			#print "Odds2 : %s" % data
-			self.__bet365 = -1
+                if self.__pinnacle == 1 and data.find("Pinnacle") :
+                        self.__pinnacle = 2
+                elif self.__pinnacle == 2  :
+                        print "datas = %s" % data
+                        odds = data.split(",")
+                        i = 0
+                        for elt in odds :
+                            catchDec = False
+                            catchUs = False
+                            oddsDec = re.search("\d+\.\d+", elt)
+                            oddsUs = re.search("[+-]\d+", elt)
+                            if oddsDec :
+                                strOdds = oddsDec.group(0)
+                                catchDec = True
+                                #print "catch :%s" % strOdds
+                            elif oddsUs :
+                                strOdds = oddsUs.group(0)
+                                #print "catch :%s" % strOdds
+                                catchUs = True
+
+                            #raw_input("next")
+                            if catchDec or catchUs:
+                                try :
+                                    tmp = float(strOdds)
+                                    self.__odds[i] = strOdds
+                                    #print "odds%d = " % i
+                                    #print "%s" % strOdds
+                                    i = i+1
+                                except ValueError:
+                                    res =  "ko"
+                            if i == 3:
+                                break
+                        #print "Odds1 : %s" % data
+                        if i == 3 :
+                            self.__pinnacle = -1
 
 
 	def handle_endtag(self, tag):
-		self.__betweenTag = False
+                self.__betweenTag = False
+                if tag == "p" and self.__pinnacle > 0:
+                        self.__pinnacle = 0
 
 
 
